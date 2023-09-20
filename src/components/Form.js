e1280b294f7ebd5db1c5d2b337404150803fd2fb
Sami <s.babouche@esi-sba.dz>
2023-09-20 15:35:23 +0100

    Completed Assingment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,24 @@
-import React, { useState } from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
 
+// src/components/Form
+import React from "react"
+
+function Form (props) {
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input
+        type="text"
+        onChange={props.handleFirstNameChange}
+        value={props.firstName}
+      />
+      <input
+        type="text"
+        onChange={props.handleLastNameChange}
+        value={props.lastName}
+      />
       <button type="submit">Submit</button>
     </form>
-  );
+  )
 }
 
 export default Form;
